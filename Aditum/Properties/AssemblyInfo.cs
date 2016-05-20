676ae45723cea3ee0f8cb7e5e465f285944e1576
F1tZy <f1tz81@gmail.com>
2016-05-19 21:57:26 -0400

    fixed up some docs

--- Aditum/Properties/AssemblyInfo.cs
+++ Aditum/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Aditum")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A light weight GUI for Monogame")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aaron Van Prooyen")]
 [assembly: AssemblyProduct("Aditum")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 The Goal of Aditum is to create a low barrier simple gui engine for Mono Game.
 Like most game devs GUIs are my least favourite things to build out. Since I 
 could not find a suitable pre made solution I decided to share mine with you.
+
+[NuGet Package](https://www.nuget.org/packages/Aditum)
